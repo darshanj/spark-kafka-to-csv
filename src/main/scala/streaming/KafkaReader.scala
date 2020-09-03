ae3bf75e4b6d012db1e1ac99cc62dc184896fa93
darshan jani <darshanjani100@gmail.com>
2020-09-03 17:43:24 +0800

    fix conflicts

--- src/main/scala/streaming/KafkaReader.scala
+++ src/main/scala/streaming/KafkaReader.scala
@@ -6,16 +6,10 @@ trait Reader {
 }
 case class KafkaReader(kafkaConfig: KafkaConfig) extends Reader {
   def spark: SparkSession = SparkSession.getActiveSession.get
-<<<<<<< HEAD
-  def read(topic:String, offsets: Offsets = new LatestAvailableOffsets()) : KakfaDataFrame = {
+  def read(topic:String, offsets: Offsets = new LatestAvailableOffsets()) : KafkaDataFrame = {
     val readOptions = offsets.options ++ kafkaConfig.options ++ Map("subscribe" -> topic,"includeTimestamp" -> "true")
 
-    KakfaDataFrame(spark.readStream
-=======
-  def read(topic:String, offsets: Offsets = new LatestAvailableOffsets()) : KafkaDataFrame = {
-    val readOptions = offsets.options ++ kafkaConfig.options ++ Map("subscribe" -> topic)
-    KafkaDataFrame(spark.read
->>>>>>> 0f336992def62982d3bfdafdd7519029bdb62d47
+    KafkaDataFrame(spark.readStream
       .format("kafka")
       .options(readOptions)
       .load())
